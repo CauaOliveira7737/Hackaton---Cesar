a41a28bae2fc3c1a41c4f75cf2d09d9e54530176
KaioM11 <kaio.ferreira23@etepd.com>
2025-06-13 09:07:09 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -17,3 +17,51 @@
     menuBtn.addEventListener('click', abrirMenu);
     fecharBtn.addEventListener('click', fecharMenu);
     overlay.addEventListener('click', fecharMenu);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.querySelector("video");
+    const playPauseBtn = document.querySelector(".playPause");
+    const playIcon = `<i class="fas fa-play"></i>`;
+    const pauseIcon = `<i class="fas fa-pause"></i>`;
+
+    video.removeAttribute("controls");
+
+    function updateIcon() {
+        playPauseBtn.innerHTML = video.paused ? playIcon : pauseIcon;
+    }
+
+    playPauseBtn.addEventListener("click", () => {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+
+        setTimeout(() => {
+            playPauseBtn.classList.add("hide");
+        }, 1500);
+    });
+
+    video.addEventListener("pause", () => {
+        updateIcon();
+        playPauseBtn.classList.remove("hide");
+    });
+
+    video.addEventListener("play", updateIcon);
+
+    // Clique na área do vídeo para reexibir o botão
+    video.addEventListener("click", () => {
+        playPauseBtn.classList.remove("hide");
+
+        // Oculta após 1.5s se estiver tocando
+        if (!video.paused) {
+            setTimeout(() => {
+                playPauseBtn.classList.add("hide");
+            }, 1500);
+        }
+    });
+});
+
+
+
+
